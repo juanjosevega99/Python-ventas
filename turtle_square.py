4f05a344aa0bd0b87ed63ff4773b06ebfb012fe6
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-02-25 20:12:17 -0500

    Proyecto de ventas con Python (basico -avanzado)

---
+++ turtle_square.py
@@ -0,0 +1,57 @@
+import turtle 
+
+window = turtle.Screen()
+tortuga = turtle.Turtle()
+
+tortuga.forward(100)
+tortuga.right(90)
+
+tortuga.forward(100)
+tortuga.right(90)
+
+tortuga.forward(100)
+tortuga.right(90)
+  
+tortuga.forward(100)
+tortuga.right(90)
+
+window.mainloop()
+# import turtle
+
+# window = turtle.Screen()
+# tortuga = turtle.Turtle()
+# tortuga.pensize(30)
+# tortuga.pencolor('#94cc3c')
+
+# tortuga.left(45)
+# tortuga.forward(80)
+
+# tortuga.left(90)
+# tortuga.forward(100)
+
+# tortuga.left(90)
+# tortuga.forward(140)
+
+# tortuga.left(90)
+# tortuga.forward(130)
+
+# tortuga.left(90)
+# tortuga.forward(40)
+
+# window.mainloop()
+
+# import turtle
+
+# window = turtle.Screen()
+
+# colors=['red','purple','blue','green','yellow','orange']
+
+# t = turtle.Pen()
+# t.speed(0)
+# for x in range(360):
+#     t.pencolor(colors[x%6])
+#     t.width(x/100+1)
+#     t.forward(x)
+#     t.left(59)
+
+# turtle.mainloop()
